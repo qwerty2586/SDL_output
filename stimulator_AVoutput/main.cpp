f0d91a185bc98e161fd354f8bb20a291477b4567
qwerty2586 <qwerty2@students.zcu.cz>
2016-10-05 02:13:15 +0200

    zakladni kostra

--- stimulator_AVoutput/main.cpp
+++ stimulator_AVoutput/main.cpp
@@ -2,12 +2,16 @@
 #include <SDL2/SDL.h>
 #include "av_screen.h"
 
-int main() {
+int main( int argc, char* args[] ) {
     if (SDL_Init( SDL_INIT_EVERYTHING )==-1) {
         std::cout << "Something went wrong!" << SDL_GetError() << std::endl;
     } else {
         AVScreen *avScreen = new AVScreen();
+        delete avScreen;
+        SDL_Quit();
+
     }
+
 
 
     return 0;
